2110b8eacbc80e9dfe2973e24864ac4a78d374e0
Michal J. Gajda <mjgajda@mail.google.com>
2013-02-13 00:13:38 +0100

    First attempt at backbone construction.

--- Topo.hs
+++ Topo.hs
@@ -18,11 +18,13 @@ type TorsionTopo   = Tree Torsion
 
 type CartesianTopo = Tree Cartesian
 
+xferC ::  Torsion -> Cartesian
 xferC tors = Cartesian { cPos     = 0
                        , cAtName  = tAtName  tors
                        , cResName = tResName tors
                        , cResId   = tResId   tors }
 
+xferT ::  Cartesian -> Torsion
 xferT cart = Torsion { tPlanar   = 0
                      , tDihedral = 0
                      , tBondLen  = 0
@@ -32,12 +34,13 @@ xferT cart = Torsion { tPlanar   = 0
 
 -- | Creates protein backbone from residue name, identifier and torsion angles.
 --   Also accepts an optional argument for next residue in chain.
-proteinBackboneT resName resId psi phi omega tail = Node n [
-                                                      Node ca [
-                                                        Node c tail,
-                                                        Node o []
-                                                              ]
-                                                           ]
+proteinBackboneT :: String-> Int-> Double-> Double-> Double-> [Tree Torsion] -> [Tree Torsion]-> Tree Torsion
+proteinBackboneT resName resId psi phi omega sc tail =
+    Node n [
+      Node ca [ -- TODO: add sidechain
+        Node c $ tail ++ [Node o []] -- TODO: should O connection have 0 degree dihedral, and reflect bond topology?
+      ]
+    ]
   where
     at planar dihe bondLen name = Torsion { tPlanar   = planar
                                           , tDihedral = dihe
@@ -47,8 +50,22 @@ proteinBackboneT resName resId psi phi omega tail = Node n [
                                           , tResId    = resId   }
     -- TODO: check that angles are not shifted
     -- 1.45:1.52:1.32
-    c  = at   111.2  phi   1.45 "C"
-    ca = at   116.2  psi   1.52 "CA"
-    n  = at   122.7  omega 1.32 "N"
-    o  = at (-120.5) omega 1.24 "N" -- TODO: check that O is indeed on the same plane as N (so it shares dihedral angle)
+    n  = at   116.2  omega 1.32 "N"
+    c  = at   122.7  phi   1.45 "C"
+    ca = at   111.2  psi   1.52 "CA"
+    o  = at (-120.5) omega 1.24 "N"  -- TODO: check that O is indeed on the same plane as N (so it shares dihedral angle.)
+-- TODO: add sidechains from: http://www.msg.ucsf.edu/local/programs/garlic/commands/dihedrals.html
+
+onlyProteinBackboneT :: String-> Int-> Double-> Double-> Double-> [Tree Torsion]-> Tree Torsion
+onlyProteinBackboneT resName resId psi phi omega tail = proteinBackboneT resName resId psi phi omega [] tail
+
+constructBackbone :: [Char] -> [(Double, Double, Double)] -> Tree Torsion
+constructBackbone seq dihedrals = head $ constructBackbone' seq dihedrals []
+-- TODO: now we are using single-letter aminoacid codes -> convert to PDB codes
+
+constructBackbone' :: [Char]-> [(Double, Double, Double)] -> [Tree Torsion] -> [Tree Torsion]
+constructBackbone' seq dihedrals = foldr1 (.) $ zipWith3 buildResidue seq [1..] dihedrals
+  where
+    -- TODO: convert resName
+    buildResidue resName resId (psi, phi, omega) tail = [onlyProteinBackboneT [resName] resId psi phi omega tail]
 
