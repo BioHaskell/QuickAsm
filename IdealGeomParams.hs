338b6f91b69e4216d73bbae8941564686a1d6749
Michal J. Gajda <mjgajda@gmail.com>
2013-02-27 13:10:09 +0100

    Corrected ideal bond angles, and lengths.

---
+++ IdealGeomParams.hs
@@ -0,0 +1,10 @@
+module IdealGeomParams(idealGeom) where
+
+{-# INLINE idealGeom #-}
+-- | Returns ideal planar angle/bond length for each atom in protein backbone (with respect to previous atom.)
+idealGeom "N"  = ( 116.2, 1.328)
+idealGeom "CA" = ( 121.7, 1.458)
+idealGeom "C"  = ( 111.2, 1.523)
+idealGeom "O"  = (-123.0, 1.231)
+-- NOTE: for backbone reconstruction to match ROSETTA's, one has to be extremely careful about angles!
+
