f05fe3a907204d97f2bab18fa78812814a606fd7
Michal J. Gajda <mjgajda@gmail.com>
2013-03-06 13:33:46 +0100

    Commented Util.Fasta.

--- Util/Fasta.hs
+++ Util/Fasta.hs
@@ -1,9 +1,11 @@
+-- | Module for conversions from single to three-letter aminoacid codes.
 module Util.Fasta(fastacode2resname) where
 
 import Data.Map as Map
 
 -- NOTE: copied from hPDB temporarily, until it is released
 
+-- | Codebook matching single and three-letter residue codes.
 codebook_standard_protein = [
   ('A', "ALA"),
   ('C', "CYS"),
@@ -29,9 +31,12 @@ codebook_standard_protein = [
   ('W', "TRP"),
   ('Y', "TYR")]
 
+-- | Dictionary of codes created from `codebook_standard_protein`.
 fastacode2resnameDictionary = Map.fromList codebook_standard_protein
 
+-- | Returns PDB three-letter code for a given single-letter FASTA code.
 fastacode2resname code    = Map.findWithDefault defaultResname   code    fastacode2resnameDictionary
 
+-- | Default three-letter code returned for unknown aminoacid code.
 defaultResname   = "UNK"
 
