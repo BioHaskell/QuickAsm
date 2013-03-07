d4fee2f122644acb10cdbf2b0de302fa8329e4ba
Michal J. Gajda <mjgajda@gmail.com>
2013-03-07 14:28:56 +0100

    Splitted computation timing into Util.Timing.

---
+++ Util/Timing.hs
@@ -0,0 +1,22 @@
+module Timing where
+
+import System.IO(hPutStrLn, stderr)
+import System.CPUTime(getCPUTime)
+import Control.DeepSeq(NFData(..), deepseq)
+import Numeric(showFFloat)
+
+time :: (NFData a) => String -> IO a -> IO a
+time name computation = time' name $ do result <- computation
+                                        result `deepseq` return result
+
+time' :: String -> IO a -> IO a
+time' name computation = do start  <- getCPUTime
+                            result <- computation
+                            end    <- getCPUTime
+                            let diff = fromIntegral (end - start) / 10^12
+                            hPutStrLn stderr $ name ++ " took " ++ showFFloat (Just 3) diff " seconds."
+                            return result
+
+timePure :: (NFData a) => String -> a -> IO a
+timePure name computation = time name (return computation)
+
--- scripts/score.hs
+++ scripts/score.hs
@@ -18,20 +18,7 @@ import Score.DistanceRestraints
 import System.CPUTime
 import Control.DeepSeq(NFData(..), deepseq)
 
-time :: (NFData a) => String -> IO a -> IO a
-time name computation = time' name $ do result <- computation
-                                        result `deepseq` return result
-
-time' :: String -> IO a -> IO a
-time' name computation = do start  <- getCPUTime
-                            result <- computation
-                            end    <- getCPUTime
-                            let diff = fromIntegral (end - start) / 10^12
-                            hPutStrLn stderr $ name ++ " took " ++ showFFloat (Just 3) diff " seconds."
-                            return result
-
-timePure :: (NFData a) => String -> a -> IO a
-timePure name computation = time name (return computation)
+import Util.Timing
 
 evaluateScores rset cart = distScore `seq` clashCount `seq` (distScore, clashCount)
   where
