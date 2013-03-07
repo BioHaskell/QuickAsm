1a6e950e26699ca426283f9bbaa4285c6e3bf7a6
Michal J. Gajda <mjgajda@gmail.com>
2013-03-07 13:11:17 +0100

    Working version of timed scoring with (hopefully) proper deepseq.

--- scripts/score.hs
+++ scripts/score.hs
@@ -15,18 +15,23 @@ import Rosetta.Restraints
 import Topo
 import Score.Steric
 import Score.DistanceRestraints
-import Data.Octree(dist)
 import System.CPUTime
-import Control.DeepSeq(deepseq)
+import Control.DeepSeq(NFData(..), deepseq)
 
-time name computation = do start  <- getCPUTime
-                           result <- deepseq computation computation
-                           end    <- getCPUTime
-                           let diff = fromIntegral (end - start) / 10^12
-                           putStrLn $ "Computing " ++ name ++ " took " ++ showFFloat (Just 3) diff " seconds."
-                           return result
+time :: (NFData a) => String -> IO a -> IO a
+time name computation = time' name $ do result <- computation
+                                        result `deepseq` return result
 
-timePure name f = time name (return f)
+time' :: String -> IO a -> IO a
+time' name computation = do start  <- getCPUTime
+                            result <- computation
+                            end    <- getCPUTime
+                            let diff = fromIntegral (end - start) / 10^12
+                            hPutStrLn stderr $ name ++ " took " ++ showFFloat (Just 3) diff " seconds."
+                            return result
+
+timePure :: (NFData a) => String -> a -> IO a
+timePure name computation = time name (return computation)
 
 evaluateScores rset cart = distScore `seq` clashCount `seq` (distScore, clashCount)
   where
@@ -34,17 +39,19 @@ evaluateScores rset cart = distScore `seq` clashCount `seq` (distScore, clashCou
     clashCount = fromIntegral $ length $ selfClashCheck $ flatten cart
 
 main = do args <- getArgs
-          when (length args /= 2) $ do hPutStrLn stderr "USAGE: checkRestraints <silent.out> <distances.cst>"
+          when (length args /= 2) $ do hPutStrLn stderr "USAGE: score <silent.out> <distances.cst>"
                                        exitFailure 
           let [ silentInputFilename, distanceRestraintsInput ] = args
-          sMdls <- processSilentFile $ BS.pack $ silentInputFilename
-          let cMdls = map (computePositions . silentModel2TorsionTopo) sMdls
-          let names = map name sMdls
+          sMdls <- time' "Parsing silent input" $ processSilentFile $ BS.pack $ silentInputFilename
+          let cMdls  = map (computePositions . silentModel2TorsionTopo) sMdls
+          let names  = map name sMdls
           let fstMdl = head cMdls
-          rset <- time "restraint set" $ prepareRestraintsFile fstMdl distanceRestraintsInput
-          forM_ (zip names cMdls) $ \(nam, cart) -> do clashCount <- timePure ("steric    scoring " ++ BS.unpack nam) $
-                                                                                  fromIntegral $ length $ selfClashCheck $ flatten cart
-                                                       distScore  <- timePure ("restraint scoring " ++ BS.unpack nam) $
+          rset <- time "Preparing restraint set" $ prepareRestraintsFile fstMdl distanceRestraintsInput
+          forM_ (zip names cMdls) $ \(nam, cart) -> do clashCount <- timePure ("Steric    scoring " ++ BS.unpack nam) $
+                                                                                  ((fromIntegral $ length $ selfClashCheck $ flatten cart) :: Double)
+                                                       distScore  <- timePure ("Restraint scoring " ++ BS.unpack nam) $
                                                                                   scoreDistanceRestraints rset cart
-                                                       putStrLn $ intercalate ", " [BS.unpack nam, show clashCount, show distScore]
+                                                       putStrLn $ intercalate ", " [ BS.unpack nam
+                                                                                   , showFFloat (Just 3) clashCount ""
+                                                                                   , showFFloat (Just 3) distScore  "" ]
 
