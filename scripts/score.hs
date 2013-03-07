14f0064461a77d12fc536fd420747b3ab898c473
Michal J. Gajda <mjgajda@gmail.com>
2013-03-07 14:48:08 +0100

    Added subrange to scripts/score.hs.

--- scripts/score.hs
+++ scripts/score.hs
@@ -25,15 +25,26 @@ evaluateScores rset cart = distScore `seq` clashCount `seq` (distScore, clashCou
     distScore  = scoreDistanceRestraints rset cart
     clashCount = fromIntegral $ length $ selfClashCheck $ flatten cart
 
+maybeSubrange :: RestraintSet -> Maybe (Int, Int) -> RestraintSet
+maybeSubrange rset Nothing       =          rset
+maybeSubrange rset (Just (a, b)) = subrange rset (a, b)
+
 main = do args <- getArgs
-          when (length args /= 2) $ do hPutStrLn stderr "USAGE: score <silent.out> <distances.cst>"
-                                       exitFailure 
-          let [ silentInputFilename, distanceRestraintsInput ] = args
+          when (length args `notElem` [2, 4]) $ do hPutStrLn stderr "USAGE: score <silent.out> <distances.cst> [<first_residue> <last_residue>]"
+                                                   hPutStrLn stderr "Where <first_residue>-<last_residue> restricts distance restraints to a subrange."
+                                                   exitFailure 
+          let [ silentInputFilename, distanceRestraintsInput ] = take 2 args
+          let maybeRestriction = case length args of
+                                   2 -> Nothing
+                                   4 -> let [a, b] = map read $ drop 2 args -- TODO: handle errors here
+                                        in Just (a, b)
+                                   _ -> error "Impossible!"
           sMdls <- time' "Parsing silent input" $ processSilentFile $ BS.pack $ silentInputFilename
           let cMdls  = map (computePositions . silentModel2TorsionTopo) sMdls
           let names  = map name sMdls
           let fstMdl = head cMdls
-          rset <- time "Preparing restraint set" $ prepareRestraintsFile fstMdl distanceRestraintsInput
+          rset' <- time     "Preparing restraint set"              $ prepareRestraintsFile fstMdl distanceRestraintsInput
+          rset  <- timePure "Extracting subrange of restraint set" $ maybeSubrange rset' maybeRestriction
           forM_ (zip names cMdls) $ \(nam, cart) -> do clashCount <- timePure ("Steric    scoring " ++ BS.unpack nam) $
                                                                                   ((fromIntegral $ length $ selfClashCheck $ flatten cart) :: Double)
                                                        distScore  <- timePure ("Restraint scoring " ++ BS.unpack nam) $
