e4e6ed986369035962b94318762ba05806f287c6
Michal J. Gajda <mjgajda@gmail.com>
2013-03-08 15:02:08 +0100

    Added a resname2fastacode method.

--- Util/Fasta.hs
+++ Util/Fasta.hs
@@ -1,7 +1,9 @@
 -- | Module for conversions from single to three-letter aminoacid codes.
-module Util.Fasta(fastacode2resname) where
+module Util.Fasta( fastacode2resname
+                 , resname2fastacode ) where
 
-import Data.Map as Map
+import qualified Data.Map as Map
+import Data.Tuple(swap)
 
 -- NOTE: copied from hPDB temporarily, until it is released
 
@@ -34,9 +36,17 @@ codebook_standard_protein = [
 -- | Dictionary of codes created from `codebook_standard_protein`.
 fastacode2resnameDictionary = Map.fromList codebook_standard_protein
 
+-- | Dictionary of residue names created from reversed `codebook_standard_protein`.
+resname2fastacodeDictionary = Map.fromList $ map swap codebook_standard_protein
+
 -- | Returns PDB three-letter code for a given single-letter FASTA code.
 fastacode2resname code    = Map.findWithDefault defaultResname   code    fastacode2resnameDictionary
+
+-- | Returns PDB single-letter FASTA code for a given three-letter code.
+resname2fastacode resname = Map.findWithDefault defaultResCode   resname resname2fastacodeDictionary
 
 -- | Default three-letter code returned for unknown aminoacid code.
 defaultResname   = "UNK"
 
+-- | Default single-letter code returned for unknown aminoacid name.
+defaultResCode   = 'X'
