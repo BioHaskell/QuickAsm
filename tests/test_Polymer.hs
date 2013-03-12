7319af6c4d60a2e9ebdab555a24e08e0632c41a5
Michal J. Gajda <mjgajda@gmail.com>
2013-03-12 18:14:41 +0100

    Draft for RepeatPolymer testing.

---
+++ tests/test_Polymer.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+-- | Tests polymer construction and instantiation.
+module Main where
+
+import Control.Exception(assert)
+
+import Rosetta.Silent
+
+import RepeatPolymer
+import Topo
+import Util.Fasta
+
+inputSilent = "examples/polymer/polymer.out"
+
+monomerLength = 61
+monomerCount  = 5
+linkerLength  = 32
+
+topo2sequence = map (resname2fastacode . tResName) . filter isCAlpha . backbone
+  where
+    isCAlpha rec = tAtName rec == "CA" 
+
+assertM condition = assert condition $ return ()
+
+computePolymerLength monomerCount monomerLength linkerLength = monomerLength * monomerCount + linkerLength * (monomerCount - 1)
+
+computePolymerSequence monomerLength linkerLength seq =
+    assertions (monomerSeq, linkerSeq)
+  where
+    (monomerSeq :: String, rest)  = splitAt monomerLength seq
+    linkerSeq   :: String         = take linkerLength     rest
+    expectedLength                = computePolymerLength monomerCount monomerLength linkerLength
+    reSeq :: String               = concat $ concat $ replicate (monomerCount - 1) [monomerSeq, linkerSeq] ++ [[monomerSeq]]
+    assertions                    = assert (expectedLength == length seq) .
+                                    assert (reSeq          == seq)        .
+                                    assert (monomerCount   >= 2)
+                        
+main = do topo <- (head . map silentModel2TorsionTopo) `fmap` processSilentFile inputSilent
+          let seq = topo2sequence topo
+          let (monoSeq, linkerSeq) = computePolymerSequence monomerLength linkerLength seq
+          print $ monomerLength * monomerCount + linkerLength * (monomerCount - 1)
+          print $ length seq
+          putStrLn seq
+          putStr "Monomer: "
+          putStrLn monoSeq
+          putStr "Linker : "
+          putStrLn linkerSeq
+          --print topo
+
+
