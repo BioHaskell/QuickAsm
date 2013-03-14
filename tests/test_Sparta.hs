92a6f87536719794d78a8fabeb3a8c07c3a3124f
Michal J. Gajda <mjgajda@mail.google.com>
2013-03-14 02:17:53 +0100

    More careful testing.

--- tests/test_Sparta.hs
+++ tests/test_Sparta.hs
@@ -6,16 +6,20 @@ import Rosetta.Silent
 
 import Topo
 import Score.Sparta
+import Util.Timing
 
 exDir = "examples/sparta"
 
 spartaOutputFilename    = exDir </> "sparta.out"
+spartaOutputFilename2   = exDir </> "sparta_asyn.out"
 silentInputFilename     = exDir </> "S_0005_3426.out"
 structureOutputFilename = exDir </> "S_0005_3426.pdb" -- used by SPARTA+ after writing
 chemShiftInputFilename  = exDir </> "asyn_gs_long.tab"
 
-main = do parseResult <- parseSpartaFile   spartaOutputFilename
+main = do parseResult  <- parseSpartaFile   spartaOutputFilename
           print parseResult
+          parseResult2 <- parseSpartaFile   spartaOutputFilename2
+          print parseResult2
           [mdl]  <- processSilentFile silentInputFilename
           let cartopo = computePositions $ silentModel2TorsionTopo mdl
           spartaResult <- runSparta chemShiftInputFilename structureOutputFilename cartopo
