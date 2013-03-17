bd68a317f5c9fc1cd5e19af200c2b56ce8eacb32
Michal J. Gajda <mjgajda@mail.google.com>
2013-03-17 23:53:40 +0100

    RepeatPolymer: sampling methods.

--- RepeatPolymer.hs
+++ RepeatPolymer.hs
@@ -1,13 +1,21 @@
 {-# OPTIONS_GHC -funbox-strict-fields #-}
 -- | Allows for making a repeat polymer.
-module RepeatPolymer( Polymer     (..)
+module RepeatPolymer( Polymer      (..)
                     , makePolymer
                     , extractPolymer
                     , glueChain -- move to 
-                    , instantiate      ) where
+                    , instantiate
+                    , PolymerModel (..)
+                    , makePolymerModel
+                    , samplePolymer
+                    , samplePolymerModel ) where
+
+import Data.Maybe(fromMaybe)
+import Control.Exception(assert)
 
 import Topo
 import FragReplace
+import Model
 
 -- * Polymer data structure and its expansion.
 -- | Holds all information necessary for constructing a polymer
@@ -27,6 +35,22 @@ makePolymer monomerTopo linkerTopo repeats = Polymer {
                                              }
   where
     lengthInResidues = tResId . last . backbone -- assuming they're numbered from 1.
+
+data PolymerModel = PModel { polymer :: Polymer
+                           , tPoly   :: TorsionTopo
+                           , cPoly   :: CartesianTopo
+                           }
+
+instance Model PolymerModel where
+  cartesianTopo = cPoly
+  torsionTopo   = tPoly
+
+makePolymerModel poly = PModel { polymer = poly
+                               , tPoly   = topo
+                               , cPoly   = computePositions topo
+                               }
+  where
+    topo = instantiate poly
 
 -- | Instantiates a polymer of N identical monomer units, linked by a linker unit.
 instantiate :: Polymer -> TorsionTopo
@@ -80,5 +104,21 @@ glueChain topo1 topo2 = result
   where
     Just result = topo1 `changeTopoAt` (\t -> tAtName t == "OXT") $ const topo2
 
-    
+samplePolymer fragSet poly gen = if pos <= monomerLen poly
+                                   then (polyXchgMono,   gen')
+                                   else (polyXchgLinker, gen')
+  where
+   polyXchgMono   = poly { monomer = xchg $ monomer poly }
+   polyXchgLinker = poly { linker  = xchg $ linker  poly }
+   -- TODO: change fromMaybe to fromJust, after cutting fragment set at right place
+   xchg topo = fromMaybe topo $ replaceFragment pos frag topo
+   ((pos, frag), gen') = randomF fragSet gen
+   assertions = assert $ pos <= monomerLen poly + linkerLen poly
 
+samplePolymerModel fragSet poly gen = PModel { polymer = poly'
+                                             , tPoly   = topo'
+                                             , cPoly   = computePositions topo'
+                                             }
+  where
+    (poly', gen') = samplePolymer fragSet poly gen
+    topo' = instantiate poly'
