8894de79c063b3e3e184a7572ed859e65082bd46
Michal J. Gajda <mjgajda@mail.google.com>
2013-04-06 20:01:17 +0200

    Util.Assert.errorWithStack.

--- Util/Assert.hs
+++ Util/Assert.hs
@@ -1,9 +1,15 @@
 -- | Utility functions for testing.
-module Util.Assert(assertM) where
+{-# OPTIONS_GHC -O0 #-}
+module Util.Assert(assertM, errorWithStack) where
 
 import Control.Exception(assert)
+import GHC.Stack(currentCallStack, renderStack)
+import System.IO.Unsafe
 
 -- | Assertion as a monadic action.
 assertM :: Monad m => Bool -> m ()
 assertM condition = assert condition $ return ()
 
+{-# NOINLINE errorWithStack #-}
+-- | Utility rendering current stack when reporting fatal error.
+errorWithStack msg = error $ msg ++ ":\n" ++ unsafePerformIO (renderStack `fmap` currentCallStack)
